b0ac644f4a9e3bb81737f93a4f1d9e3a8a26f1fc
Anna Rogers <2muddybarefeet@gmail.com>
2016-08-06 11:59:30 -0700

    add country overlay

--- overlayPractice/ViewController.swift
+++ overlayPractice/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var worldMap: MKMapView!
+    
+    var Mali = Country()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("mali", Mali)
+        addBoundary()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addBoundary() {
+        let polygon = MKPolygon(coordinates: &Mali.boundary, count: Mali.boundaryPointsCount)
+        worldMap.addOverlay(polygon)
     }
-
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        //let overlayView: MKOverlayRenderer?
+        if overlay is MKPolygon {
+            let polygonView = MKPolygonRenderer(overlay: overlay)
+            polygonView.strokeColor = UIColor.magentaColor()
+            polygonView.fillColor = UIColor.greenColor()
+            polygonView.alpha = 0.5
+            return polygonView
+        }
+        return MKOverlayRenderer()
+    }
 
 }
+
 
