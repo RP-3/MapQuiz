c13eee7d19a6e8bb196a4aae4a6e455374fe52ba
Anna Rogers <2muddybarefeet@gmail.com>
2016-08-07 07:19:30 -0700

    update the model

--- overlayPractice/LandArea+CoreDataProperties.swift
+++ overlayPractice/LandArea+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  LandArea+CoreDataProperties.swift
 //  
 //
-//  Created by Anna Rogers on 8/6/16.
+//  Created by Anna Rogers on 8/7/16.
 //
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,9 +14,10 @@ import CoreData
 
 extension LandArea {
 
-    @NSManaged var name: String?
-    @NSManaged var continent: String?
     @NSManaged var capital: String?
+    @NSManaged var continent: String?
     @NSManaged var coordinates: String?
+    @NSManaged var name: String?
+    @NSManaged var coordinate_type: String?
 
 }
--- overlayPractice/LandArea.swift
+++ overlayPractice/LandArea.swift
@@ -2,7 +2,7 @@
 //  LandArea.swift
 //  
 //
-//  Created by Anna Rogers on 8/6/16.
+//  Created by Anna Rogers on 8/7/16.
 //
 //
 
@@ -11,16 +11,18 @@ import CoreData
 
 
 class LandArea: NSManagedObject {
-
-    convenience init(name: String, continent: String, coordinates: String,  context : NSManagedObjectContext){
+    
+    convenience init(name: String, continent: String, coordinates: String, coordType: String,  context : NSManagedObjectContext){
         if let ent = NSEntityDescription.entityForName("LandArea",inManagedObjectContext: context){
             self.init(entity: ent, insertIntoManagedObjectContext: context)
             self.name = name
             self.continent = continent
             self.coordinates = coordinates
+            self.coordinate_type = coordType
         }else{
             fatalError("Unable to find Entity name!")
         }
     }
     
 }
+
